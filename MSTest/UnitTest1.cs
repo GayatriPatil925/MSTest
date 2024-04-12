ef20cc51e59708ec21c0e3935597c6fb79786593
gayatri0987 <Gayatri.Ghadekar@mindtree.com>
2024-04-12 21:12:50 +0530

    Add project files.

---
+++ MSTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [ClassInitialize]
+        public void initialize()
+        {
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
