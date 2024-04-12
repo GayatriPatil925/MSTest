18674a5d3ca98fc0e8cc07048f48d96f4231a6c8
gayatri0987 <Gayatri.Ghadekar@mindtree.com>
2024-04-12 22:03:55 +0530

    MSTest Practice

--- MSTest/UnitTest1.cs
+++ MSTest/UnitTest1.cs
@@ -1,16 +1,40 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace MSTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [ClassInitialize]
-        public void initialize()
-        {
+        public static IWebDriver driver;
 
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        { 
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://demoqa.com/";
         }
-        [TestMethod]
-        public void TestMethod1()
+
+        [ClassCleanup]
+        public void Cleanup()
         {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        //[Priority(0)]
+        public void AccordianVerification()
+        {
+            HomePage homePage = new HomePage(driver);
+            homePage.NevigateToElements("Widgets");
+
+            Widgets widgets=new Widgets(driver);
+            widgets.NavigateToWidgetsType("Accordian");
+
+            string AC = "Accordian";
+            Assert.AreEqual(AC, widgets.WidgetsVerification("Accordian"));
         }
     }
 }
---
+++ MSTest/Widgets.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTest
+{
+    public class Widgets
+    {
+        public IWebDriver driver;
+        public IJavaScriptExecutor js;
+
+        public Widgets(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+            js=(IJavaScriptExecutor)driver;
+        }
+
+        public void NavigateToWidgetsType(string eleType)
+        {
+            IWebElement elements = driver.FindElement(By.XPath("//span[@class='text' and contains(text(),'" + eleType + "')]"));
+            js.ExecuteScript("arguments[0].scrollIntoView(true)", elements);
+            elements.Click();
+        }
+
+        public string WidgetsVerification(string eleType)
+        {
+            return  driver.FindElement(By.XPath("//h1[contains(text(),'"+eleType+"')]")).Text;
+        }
+    }
+}
